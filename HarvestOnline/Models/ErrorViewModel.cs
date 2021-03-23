3a35d6df67f5888509a79121d1cceb295d401193
michaelylanan <michaeljr.ylanan@benilde.edu.ph>
2021-03-23 08:33:04 +0800

    Initial Commit

---
+++ HarvestOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HarvestOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
