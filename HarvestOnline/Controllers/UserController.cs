e9b6a55580be98f40ffef1df33198c4688220f66
michaelylanan <michaeljr.ylanan@benilde.edu.ph>
2021-03-29 15:35:06 +0800

    Initial Commit

---
+++ HarvestOnline/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using HarvestOnline.Data;
+using HarvestOnline.Models;
+
+using System.IO;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+
+namespace HarvestOnline.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
