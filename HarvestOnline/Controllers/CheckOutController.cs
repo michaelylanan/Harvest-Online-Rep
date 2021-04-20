f7ec2b244872747b02acc0b3b64ba7c1982aa67d
Miguel Magallanes <miguelryan.magallanes@benilde.edu.ph>
2021-04-20 19:28:41 +0800

    Email message

--- HarvestOnline/Controllers/CheckOutController.cs
+++ HarvestOnline/Controllers/CheckOutController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 using System.Net;
 using System.Net.Mail;
 using HarvestOnline.Models;
@@ -38,18 +39,33 @@ namespace HarvestOnline.Controllers
         [HttpPost]
         public IActionResult Mail(Mail record)
         {
-            using (MailMessage mail = new MailMessage("miguelryan.magallanes@benilde.edu.ph", record.Email))
+            using (MailMessage mail = new MailMessage("harvestonline888@gmail.com", record.Email))
             {
                 mail.Subject = "Order Notification";
-                string message = "Hello";
+                string message = "Hello," + "<br/><br/><br/>" + "Thank you for ordering from Harvest Onine"+ "<br/><br/>"
+                                         + "Your order is currently being processed " + "<br/><br/><br/>"
+                                         + "Order created at " + DateTime.Now.ToString("M/d/yyyy");
 
                 mail.Body = message;
                 mail.IsBodyHtml = true;
 
-    
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.UseDefaultCredentials = false;
+                    NetworkCredential NetworkCred = new NetworkCredential("harvestonline888@gmail.com", "Harvest*15");
+                    smtp.EnableSsl = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = 587;
+                    smtp.Send(mail);
+                    ViewBag.Message = "Inquiry Sent";
+                }
             }
-                return View();
+             return View();
         }
+
+              
+        
 
     }
 }
